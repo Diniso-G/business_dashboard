fec5968644afae4f001db526461eb3d5068db945
Diniso-G <402308838@my.richfield.ac.za>
2026-06-29 10:16:07 +0200

    set up jwt key

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 from datetime import datetime
 
 DATABASE_URL = "sqlite:///./dashboard.db"
@@ -7,11 +7,22 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind = engine)
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+    reports = relationship("Report", back_populates="owner")
+
 class Report(Base):
     __tablename__ = "reports"
     id = Column(Integer, primary_key=True)
     filename = Column(String)
     total_revenue = Column(Float)
     uploaded_at = Column(DateTime, default=datetime.utcnow)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="reports")
 
 Base.metadata.create_all(engine)
