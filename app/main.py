e4f5f79595b46f59cc8f79ef1084925e4d8762fb
Diniso-G <402308838@my.richfield.ac.za>
2026-07-02 13:27:16 +0200

    Improve UI ongoing

--- app/main.py
+++ app/main.py
@@ -9,13 +9,14 @@ from app.auth_routes import router as ar
 from fastapi.responses import JSONResponse
 import traceback
 
+from fastapi.staticfiles import StaticFiles
+
 app = FastAPI(title="Business Analytics Dashboard")
-app.mount("/static", StaticFiles(), name = "static")
+app.mount("/static", StaticFiles(directory="static"), name = "static")
 app.include_router(router)
 app.include_router(ar)
 
 @app.exception_handler(Exception)
 async def debug_exception_handler(request, exc):
-    return JSONResponse(status_code=500, content={"error": str(exc), "traceback": traceback.format_exc()}
-                        )
+    return JSONResponse(status_code=500, content={"error": str(exc), "traceback": traceback.format_exc()})
 
